5964cb804351afe5f8cc5c73bb3b59cc280f952c
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-04-02 12:58:17 -0400

    created nav links and setup client side routing

--- budget-io/src/App.js
+++ budget-io/src/App.js
@@ -5,6 +5,7 @@ import Create from './components/Create';
 import Help from './components/Help';
 import Edit from './components/Edit';
 import NotFound from './components/NotFound';
+import Header from './components/Header';
 
 import './App.css';
 
@@ -12,11 +13,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/create" component={Create} />
           <Route path="/help" component={Help} />
-          <Route path="/edit" component={Edit} />
+          <Route path="/edit/:id" component={Edit} />
           <Route component={NotFound} />
         </Switch>
       </div>
--- budget-io/src/index.js
+++ budget-io/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-
-import './index.css';
 import App from './App';
+
+import './Styles/css/index.css';
+import './index.css';
 
 ReactDOM.render(
   <Router>
