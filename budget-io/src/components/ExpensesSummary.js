273e816abd82d6522ce70ce7b5537e92a1046592
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-04-15 19:09:55 -0400

    converted to class component

--- budget-io/src/components/ExpensesSummary.js
+++ budget-io/src/components/ExpensesSummary.js
@@ -4,15 +4,24 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
-  const expenseWord = expensesCount === 1 ? 'expense' : 'expenses';
-  const formattedExpenesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+class ExpensesSummary extends React.Component {
+  constructor({ expensesCount, expensesTotal }) {
+    super({ expensesCount, expensesTotal });
+      this.state = {
+        expensesCount: expensesCount,
+        expensesTotal: expensesTotal
+      }
+  }
 
-  return (
-    <div>
-      <h1>Viewing {expensesCount} {expenseWord} totalling {formattedExpenesTotal}</h1>
-    </div>
-  )
+  render () {
+    const expenseWord = this.expensesCount === 1 ? 'expense' : 'expenses';
+    const formattedExpenesTotal = numeral(this.state.expensesTotal / 100).format('$0,0.00');
+    return (
+      <div>
+        <h1>Viewing {this.state.expensesCount} {expenseWord} totalling {formattedExpenesTotal}</h1>
+      </div>
+    )
+  }
 }
 
 const mapeStateToProps = (state) => {
