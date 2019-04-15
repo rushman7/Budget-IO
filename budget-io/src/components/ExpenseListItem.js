fe64e58206b4737dde797caf13c61a67aa9503e8
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-04-15 14:03:44 -0400

    format changes

--- budget-io/src/components/ExpenseListItem.js
+++ budget-io/src/components/ExpenseListItem.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { removeExpense } from '../actions/expenses';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, dispatch, description, amount, createdAt }) => (
   <div>
     <h3>{description}</h3>
-    <p>{amount} - {createdAt}</p>
+    <p>{numeral(amount / 100).format('$0,0.00')} - {moment(createdAt).format('MMM Do, YYYY')}</p>
     <Link to={`/edit/${id}`}>
       <button>Edit</button>
     </Link>
