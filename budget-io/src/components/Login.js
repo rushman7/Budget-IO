7a27f0a72a3eaa56c60f305910d8ba0ddd01fa9d
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-04-21 01:01:34 -0400

    login page css done

--- budget-io/src/components/Login.js
+++ budget-io/src/components/Login.js
@@ -14,18 +14,21 @@ const Login = ({
   startGithubLogin, 
   startUserLogin
 }) => (
-  <div>
-    <button onClick={startGoogleLogin}>Login Google</button>
-    <button onClick={startFacebookLogin}>Login Facebook</button>
-    <button onClick={startGithubLogin}>Login Github</button>
-    <form onSubmit={startUserLogin}>
-      <input type="text" placeholder="Email" value={startUserLogin.email}/>
-      <input type="text" placeholder="Password" value={startUserLogin.pass}/>
-      <button>Login</button>
-    </form>
-    <Link to="/signup">
-      <button>Register</button>
-    </Link>
+  <div className="login_shell">
+    <div className="login_container">
+      <img className="img-logo" src="https://i.imgur.com/94GQoWA.png" alt="logo"/>
+      <form className="login_form" onSubmit={startUserLogin}>
+        <input className="login_input" type="email" placeholder="Email" value={startUserLogin.email}/>
+        <input className="login_input" type="password" placeholder="Password" value={startUserLogin.pass}/>
+        <button className="login_button">Login</button>
+      </form>
+      <Link to="/signup" style={{ textDecoration: 'none' }}>
+        <button className="login_button">Register</button>
+      </Link>
+      <i className="fab fa-facebook-square fa-3x" onClick={startFacebookLogin}></i>
+      <i className="fab fa-google fa-3x" onClick={startGoogleLogin}></i>
+      <i className="fab fa-github fa-3x" onClick={startGithubLogin}></i>
+    </div>
   </div>
 );
 
